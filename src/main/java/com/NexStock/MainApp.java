fe24d9f446a115b15e1d92456dd6932c4771cf89
Hanan Qaisar (Linux) <hananqaisar316@gmail.com>
2026-05-15 16:34:29 +0500

    Modified mainapp

--- src/main/java/com/NexStock/MainApp.java
+++ src/main/java/com/NexStock/MainApp.java
@@ -1,36 +1,19 @@
 package com.NexStock;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import com.NexStock.controller.Sceneswitches;
 
 public class MainApp extends Application {
 
     @Override
-    public void start(Stage stage) {
-        try {
-
-            FXMLLoader loader = new FXMLLoader(
-                    getClass().getResource("/com/NexStock/view/createAccount.fxml"));
-
-
-// Add a null check to debug easily
-            if (loader.getLocation() == null) {
-                System.out.println("FXML file not found!");
-                return;
-            }
-            Scene scene = new Scene(loader.load());
-            stage.setTitle("Create Account");
-
-            stage.setScene(scene);
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void start(Stage stage) {//here a controller object is created
+        System.out.println("App starting, setting stage... let's gooo (' ? ')");//Just for fun
+        Sceneswitches.setStage(stage);
+        Sceneswitches.now_switchin("createAccount.fxml");
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch(args);//it start my app
     }
 }
