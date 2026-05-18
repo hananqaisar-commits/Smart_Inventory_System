94159fb5dea48c9150305a299ef01021f631bda1
Waqar <waqaryou06@gmail.com>
2026-05-18 21:37:21 +0500

    Added report controller class

---
+++ src/main/java/com/NexStock/controller/ReportController.java
@@ -0,0 +1,106 @@
+package com.NexStock.controller;
+
+
+import com.NexStock.report.ReportManager;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import java.time.LocalDate;
+
+public class ReportController {
+
+    @FXML
+    private TextField invoiceIDField;
+
+    @FXML
+    private Button btnSingleBill;
+
+    @FXML
+    private Button btnDailyReport;
+
+    @FXML
+    private Button btnProductWise;
+
+    @FXML
+    private Button btnALlHistory;
+
+    @FXML
+    private Button btnGenerate;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private TextArea reportArea;
+
+    ReportManager manager = new ReportManager();
+    private String SelectedBtn;
+
+    @FXML
+    public void initialize() {
+        datePicker.setValue(LocalDate.now());
+        reportArea.setText("");
+        reportArea.setEditable(false);
+        btnSingleBill.setOnAction(e -> handleSingleBill());
+        btnDailyReport.setOnAction(e -> handleDailyReport());
+        btnProductWise.setOnAction(e -> handleProductWise());
+        btnALlHistory.setOnAction(e -> handleAllHistory());
+        btnGenerate.setOnAction(e -> handleGenerate());
+    }
+
+    public void handleSingleBill() {
+        SelectedBtn = "Single";
+    }
+
+    public void handleDailyReport() {
+        SelectedBtn = "DailyReport";
+    }
+
+    public void handleProductWise() {
+        SelectedBtn = "ProductWise";
+    }
+
+
+    public void handleAllHistory() {
+        SelectedBtn = "AllHistory";
+    }
+
+
+    public void handleGenerate() {
+
+        if (SelectedBtn == null) {
+            reportArea.setText("Please select a report type  first!");
+            return;
+        }
+
+        if (SelectedBtn.equals("Single")){
+           if (invoiceIDField.getText()==null || invoiceIDField.getTeaxt().trim().isEmpty()){
+                                reportArea.setText("Please Enter Invoice ID!");
+           return;
+           }
+        }
+           if(SelectedBtn.equals("DailyReport")) {
+               if(datePicker.getValue()==null){
+                   reportArea.setText("Please select a Date!");
+               return;
+               }
+           }
+
+        if (SelectedBtn.equals("Single")) {
+            String receipt = manager.getSingleBillReceipt(invoiceIDField.getText());
+            reportArea.setText(receipt);
+        } else if (SelectedBtn.equals("DailyReport")) {
+            String report = manager.generateDailyReport(datePicker.getValue());
+            reportArea.setText(report);
+        } else if
+        (SelectedBtn.equals("ProductWise")) {
+            String report = manager.generateProductWiseReport();
+            reportArea.setText(report);
+        } else if
+        (SelectedBtn.equals("AllHistory")) {
+            String report = manager.generateAllHistoryReport();
+            reportArea.setText(report);
+        }
+
+    }
+}
